f36b68a7fc1decd51b11ea08cb5908ca4c42f8d4
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-04-21 13:52:53 +0200

    reshape numbers array

--- mean_var_std.py
+++ mean_var_std.py
@@ -3,3 +3,6 @@ import numpy as np
 def calculate(list):
   if len(list) is not 9:
     raise ValueError('List have to be nine numbers long')
+
+  numbers = np.array(list).reshape(3,3)
+  print(numbers)
