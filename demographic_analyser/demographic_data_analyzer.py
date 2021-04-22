3d986129b53a3e93045bdfeb8c2d4c281cf0dabd
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-04-22 11:58:17 +0200

    Added comment to prints for dev

--- demographic_analyser/demographic_data_analyzer.py
+++ demographic_analyser/demographic_data_analyzer.py
@@ -53,7 +53,7 @@ def calculate_demographic_data(print_data=False):
         print("Country with highest percentage of rich:", highest_earning_country)
         print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
         print("Top occupations in India:", top_IN_occupation)
-
+'''
     return {
         'race_count': race_count,
         'average_age_men': average_age_men,
