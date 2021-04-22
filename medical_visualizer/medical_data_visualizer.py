e24bfb0ba3e5d41b445ec0e17f5df0278c0eeb71
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-04-22 17:37:35 +0200

    Cleaning the data with quantiles

--- medical_visualizer/medical_data_visualizer.py
+++ medical_visualizer/medical_data_visualizer.py
@@ -41,7 +41,11 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & 
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) & 
+        (df['weight'] <= df['weight'].quantile(0.975))]
 
     # Calculate the correlation matrix
     corr = None
