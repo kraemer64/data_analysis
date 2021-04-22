a55979e9213614df971a83513d92b117c48711dd
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-04-22 12:15:19 +0200

    get exclusive age of all men

--- demographic_analyser/demographic_data_analyzer.py
+++ demographic_analyser/demographic_data_analyzer.py
@@ -4,16 +4,14 @@ import pandas as pd
 def calculate_demographic_data(print_data=False):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
-    print(df.head(), '\n')
-    print(df.shape, '\n')
     print(df.columns.values, '\n')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = df['race'].value_counts()
-    print(race_count)
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df.loc[df['sex'] == 'Male', 'age']
+    print(average_age_men)
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = None
