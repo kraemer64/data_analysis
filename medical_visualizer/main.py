bb89272e7d59c2eda709c739d578691a325d5954
kraemer64 <54526557+kraemer64@users.noreply.github.com>
2021-04-22 16:30:32 +0200

    Disabled testing for dev, installed seaborn

--- medical_visualizer/main.py
+++ medical_visualizer/main.py
@@ -7,4 +7,4 @@ medical_data_visualizer.draw_cat_plot()
 medical_data_visualizer.draw_heat_map()
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+# main(module='test_module', exit=False)
